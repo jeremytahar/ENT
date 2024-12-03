5b64fe307474b6ae7baa268d97870ca87b314e8b
Amin <amin.benazzouz@edu.univ-eiffel.fr>
2024-12-03 16:00:55 +0100

    création du formulaire de connexion

---
+++ app/view/LogIn.php
@@ -0,0 +1,22 @@
+<div class="connexion_page">
+    <h1><span class="logo_title"><img src="img/logo.svg"></span>GUSTAVE EIFFEL</h1>
+
+    <form class="form_connexion" action="index.php" method="POST">
+        <h2><span><img src="img/logo.svg"></span>CONNEXION</h2>
+
+        <label>Identifiant</label>
+        <input type="text" name="pseudo" autocomplete="off">
+
+                <br>
+
+        <label>Mot de Passe</label>
+        <input type="password" name="password" autocomplete="off">
+
+                <br>
+                <br>
+        
+        <input type="submit" name="submit" value="Se connecter">
+
+    </form>
+    
+</div>
